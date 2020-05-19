e5785c244ef53f1c1c3f9be49c21fd0c076e1e5e
Ilya Lebedev <lebiafan42@gmail.com>
2020-05-20 00:05:12 +0300

    Task complite

--- src/main/java/ru/ifmo/collections/FirstUnique.java
+++ src/main/java/ru/ifmo/collections/FirstUnique.java
@@ -1,19 +1,35 @@
 package ru.ifmo.collections;
 
+import java.util.*;
+
 /**
  * Design a class which contains integers and returns first unique integer (in order of addition).
  * FirstUniqueTest can be used as an example.
  */
 public class FirstUnique {
+    private final HashMap<Integer, Boolean> uniqueness;
+    private final Queue<Integer> order;
+
     public FirstUnique(int[] numbers) {
-        // TODO implement
+        if (numbers == null) {
+            throw new NullPointerException();
+        }
+        uniqueness = new HashMap<>();
+        order = new LinkedList<>();
+        for (int e : numbers) {
+            add(e);
+        }
     }
 
     public int showFirstUnique() {
-        throw new UnsupportedOperationException(); // TODO implement
+        while (!order.isEmpty() && !uniqueness.get(order.peek())) {
+            order.poll();
+        }
+        return order.peek() == null ? -1 : order.peek();
     }
 
-    public void add(int value) {
-        throw new UnsupportedOperationException(); // TODO implement
+    public void add(int value) throws NullPointerException {
+        uniqueness.put(value, !uniqueness.containsKey(value));
+        order.add(value);
     }
 }
--- src/main/java/ru/ifmo/collections/LruCache.java
+++ src/main/java/ru/ifmo/collections/LruCache.java
@@ -1,29 +1,42 @@
 package ru.ifmo.collections;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Represents LRU cache with fixed maximum capacity.
- *
+ * <p>
  * get() should return null if there is no value for a given key.
  * elements() should return number of elements in cache.
- *
+ * <p>
  * This class should not have any other public methods.
- *
+ * <p>
  * Implementing this cache in (almost) the same manner as it was implemented during the lecture will result in extra points.
  */
-public class LruCache<K, V> {
+public class LruCache<K, V> extends LinkedHashMap {
+    private int capacity;
+
     public LruCache(int capacity) {
-        // TODO implement
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
     }
 
-    public V get(K key) {
-        throw new UnsupportedOperationException(); // TODO implement
+    @Override
+    protected boolean removeEldestEntry(Map.Entry eldest) {
+        return size() > capacity;
     }
 
-    public void put(K key, V value) {
-        // TODO implement
+    @Override
+    public Object get(Object key) {
+        return super.get(key);
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return super.put(key, value);
     }
 
     public int elements() {
-        throw new UnsupportedOperationException(); // TODO implement
+        return size();
     }
 }
